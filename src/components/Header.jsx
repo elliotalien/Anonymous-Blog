26bd16ae7bfad5cad5df71de3ab8034652fec8ec
Abhijith P <blackdevilhacker4@gmail.com>
2024-05-23 23:16:49 +0530

    Anonymous-Blog

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="w3-container w3-center w3-padding-32">
       <h1><b>Anonymous Blog</b></h1>
-      <p>Welcome to the blog of <a style={{textDecoration:"none"}} href="/Anonymous-Blog/Dashboard" className="w3-tag">Anonymous</a></p>
+      <p>Welcome to the blog of <Link style={{ textDecoration: "none" }} to="/Dashboard" className="w3-tag">Anonymous</Link></p>
     </header>
   );
 };
 
 export default Header;
-
-
